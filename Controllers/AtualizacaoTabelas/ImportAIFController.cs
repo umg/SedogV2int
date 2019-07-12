a36cc6fb76cf1212c2b39f6e41fe8308ac5c36b3
LeiteI <LeiteI@BRRIOWD03292>
2019-07-12 16:07:37 -0300

    Versão - Inclui import AIF

---
+++ Controllers/AtualizacaoTabelas/ImportAIFController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SEDOGv2.Helpers;
+using SEDOGv2.Models;
+using System.IO;
+
+
+namespace SEDOGv2.Controllers.AtualizacaoTabelas
+{
+    public class ImportAIFController : Controller
+    {
+        // GET: ImportAIF
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            ImportAIF ret = new Models.ImportAIF();
+            try
+            {
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/Files/"), fileName);
+                        file.SaveAs(path);
+
+                        ProcessAIF psc = new ProcessAIF();
+                        ret = psc.ProcessAif(path);
+
+                        ViewBag.FileName = fileName;
+                    }
+                }
+            }
+                catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View(ret);
+        }
+
+    }
+}
---
+++ Models/ImportAIF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEDOGv2.Models
+{
+    public class ImportAIF
+    {
+        public int Linhas { get; set; }
+        public string R2Projects { get; set; }
+        public string ForeignIncome { get; set; }
+        public string ArtistRoyalties { get; set; }
+        public string ProducerRoyalties { get; set; }
+        public string OtherRoyalty { get; set; }
+        public string AllRoyalty { get; set; }
+        public string ForeignMargin { get; set; }
+        public string PercAIFMargin { get; set; }
+
+    }
+}
