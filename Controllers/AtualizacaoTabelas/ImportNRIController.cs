3bcb58af74ab78e67a874fcd168ec536c538c451
LeiteI <LeiteI@BRRIOWD03292>
2019-09-06 10:53:16 -0300

    Inclusao - Imporatação Tbl NRI

---
+++ Controllers/AtualizacaoTabelas/ImportNRIController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SEDOGv2.Helpers;
+using SEDOGv2.Models;
+using System.IO;
+using System.Data;
+
+namespace SEDOGv2.Controllers.AtualizacaoTabelas
+{
+    public class ImportNRIController : Controller
+    {
+        // GET: ImportNI
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/Files/"), fileName);
+                        file.SaveAs(path);
+
+                        ProcessNRI psc = new ProcessNRI();
+                        dt = psc.ProcessaNRI(path);
+
+                        List<ImportNRI> nriList = new List<ImportNRI>();
+
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            ImportNRI ret = new ImportNRI();
+
+                            ret.IdProjetoSedog = dt.Rows[i]["IDPROJ_SEDOG"].ToString();                            
+                            ret.R2Projects = dt.Rows[i]["R2_PROJECT"].ToString();
+                            ret.Advance = dt.Rows[i]["Advance"].ToString();
+                            ret.Recoupable = dt.Rows[i]["Recoupable"].ToString();                            
+
+                            nriList.Add(ret);
+                        }
+
+                        ViewBag.FileName = fileName;
+                        ViewBag.processedNRIList = nriList;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View(ViewBag.processedNRIList);
+        }
+    }
+}
---
+++ Models/ImportNRI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEDOGv2.Models
+{
+    public class ImportNRI
+    {
+        public int Linhas { get; set; }
+        public string IdProjetoSedog { get; set; }
+        public string R2Projects { get; set; }
+        public string Advance { get; set; }
+        public string Recoupable { get; set; }
+
+    }
+}
